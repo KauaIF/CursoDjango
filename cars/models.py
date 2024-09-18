1d86a470a8f116146bba165c76f9b82db7f47c97
KauaIF <kaua271203@gmail.com>
2024-09-18 11:15:21 -0300

    Passo 39

--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_query_name='car_brand')#poderia ser on delete cascade tbm para deletar em cascata
     factory_year = models.IntegerField(blank=True,null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True,null=True)
